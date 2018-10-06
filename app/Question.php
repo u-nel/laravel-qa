09c4878cdf37fac5e6d5b1e9738bfd546f797a4e
Lyoniel <lyoniel.farase@yahoo.com>
2018-10-06 15:46:41 +0800

    Add votes, answers, and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -19,11 +19,24 @@ class Question extends Model
         $this->attributes['slug'] = str_slug($value);
     }
 
-    public function getUrlAttribute(){
+    public function getUrlAttribute()
+    {
         return route("questions.show", $this->id);
     }
 
-    public function getCreatedDateAttribute(){
+    public function getCreatedDateAttribute()
+    {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0) {
+            if($this->best_answer_id){
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
     }
 }
