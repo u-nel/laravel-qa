bf325b145d5f93b2e31d756f644518939c98db4b
Lyoniel <lyoniel.farase@yahoo.com>
2018-10-08 13:12:01 +0800

    Implement form validation and store the question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -14,6 +14,7 @@
                     </div>
                 </div>
 
+                @include('layouts._messages')
                 <div class="card-body">
                     @foreach($questions as $question)
                         <div class="media">
