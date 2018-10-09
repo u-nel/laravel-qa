e6270e4b7496b1cef3d75a45bf4c315d4281781a
Lyoniel <lyoniel.farase@yahoo.com>
2018-10-09 13:40:41 +0800

    Display answers for question

--- app/User.php
+++ app/User.php
@@ -37,8 +37,18 @@ class User extends Authenticatable
         return "#";
     }    
 
+    
+
     public function answers()
     {
         return $this->hasMany(Answer::class);
+    }
+
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 40;
+
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
     }
 }
