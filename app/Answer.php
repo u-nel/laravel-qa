b5ecdc69ae655baf8102920eea702aeb5bd4e915
Lyoniel <lyoniel.farase@yahoo.com>
2018-10-10 13:09:45 +0800

    Update best_answer_id on questions table when an answer removed using Eloquent event"

--- app/Answer.php
+++ app/Answer.php
@@ -27,6 +27,10 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
     public static function boot()
     {
         parent::boot();
@@ -36,7 +40,13 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
         });
 
         
