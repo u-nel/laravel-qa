41afd32de7a1e3288a724f3ea078396b4127cafd
Lyoniel <lyoniel.farase@yahoo.com>
2018-10-11 16:25:46 +0800

    Make the vote answer working"

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -10,15 +10,29 @@
                 @foreach ($answers as $answer)
                     <div class="media">
                         <div class="d-flex flex-column vote-controls">
-                            <a title="This answer is useful" class="vote-up">
+                            <a title="This answer is useful"
+                            class="vote-up {{ Auth::guest() ? 'off': '' }}"
+                            onclick="event.preventDefault(); document.getElementById('upvote-answer-{{$answer->id}}').submit();"
+                            >
                                 <i class="fas fa-caret-up fa-3x"></i>
                             </a>
-                            <span class="vote-count">11</span>
-                            <a title="This answer is not useful" class="vote-down off">
+                            <form action="/answers/{{ $answer->id }}/vote" id="upvote-answer-{{ $answer->id }}" method="post" class="hidden">
+                                @csrf
+                                <input type="hidden" name="vote" value="1">
+                            </form>
+                            <span class="vote-count">{{ $answer->votes_count }}</span>
+                            <a title="This answer is not useful"
+                                class="vote-down {{ Auth::guest() ? 'off': '' }}"
+                                onclick="event.preventDefault(); document.getElementById('downvote-answer-{{$answer->id}}').submit();"
+                                >
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
+                            <form action="/answers/{{ $answer->id }}/vote" id="downvote-answer-{{ $answer->id }}" method="post" class="hidden">
+                                @csrf
+                                <input type="hidden" name="vote" value="-1">
+                            </form>
                             @can ('accept', $answer)
-                            <a title="Mark this answer as best answer" 
+                            <a title="Mark this answer as best answer"
                                class="{{ $answer->status }} mt-2 favorited"
                                onclick="event.preventDefault(); document.getElementById('accept-answer-{{$answer->id}}').submit();"
                                >
@@ -29,7 +43,7 @@
                             </form>
                             @else
                                 @if($answer->is_best)
-                                <a title="The question owner accepted this answer as best answer" 
+                                <a title="The question owner accepted this answer as best answer"
                                     class="{{ $answer->status }} mt-2 favorited"
                                     >
                                         <i class="fas fa-check fa-2x"></i>
@@ -43,7 +57,7 @@
                                 <div class="col-4">
                                     <div class="ml-auto">
                                         @can ('update', $answer)
-                                        <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>                                    
+                                        <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
                                         @can ('delete', $answer)
                                         <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="post">
@@ -65,11 +79,11 @@
                                     </a>
                                     <div class="media-body mt-1">
                                         <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                                    </div> 
-                                </div>   
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        
+
                     </div>
                     <hr/>
                 @endforeach
